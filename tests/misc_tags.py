db0eaf0ea01379264427aa889e6d71741913dfc0
Alvaro J. Genial <genial@alva.ro>
2014-04-16 15:27:26 -0400

    Add misc tag tests

---
+++ tests/misc_tags.py
@@ -0,0 +1,23 @@
+
+from django.template import Template, Context
+from django.conf import settings
+settings.configure()
+
+c = Context({})
+
+def test(s):
+    print s, '=', Template("`" + s + "`").render(c)
+
+test("{% widthratio 100 100 100 %}")
+test("{% widthratio 100 100 200 %}")
+test("{% widthratio 100 100 300 %}")
+test("{% widthratio 100 300 200 %}")
+test("{% widthratio 100 200 100 %}")
+test("{% widthratio 100 200 200 %}")
+test("{% widthratio 100 200 300 %}")
+test("{% widthratio 200 100 300 %}")
+test("{% widthratio 200 300 100 %}")
+test("{% widthratio 300 100 100 %}")
+test("{% widthratio 300 100 200 %}")
+test("{% widthratio 300 100 300 %}")
+test("{% widthratio 300 200 100 %}")
