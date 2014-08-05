de78a04728c8c65ec3d4b92798764cc0a30e3fbc
Alvaro J. Genial <genial@alva.ro>
2014-08-05 18:41:12 -0400

    Bump version to 0.7.0

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ requirements = [
 
 setup(
     name = 'django-synth',
-    version = '0.6.1',
+    version = '0.7.0',
     description = 'A Simple Integration Between Django and Synth',
     long_description = long_description,
     keywords = 'django, tmpl, ssi, template, framework',
