e1cef66446b3252f77845da887860697d4b7151e
Alvaro J. Genial <genial@alva.ro>
2014-09-03 17:17:06 -0400

    Rename to custom_simple_tags

--- tests/custom_simple_tags.py
+++ tests/custom_simple_tags.py
@@ -8,12 +8,12 @@ register = template.Library()
 def identity(arg):
     return arg
 
-template.libraries['custom_test'] = register
+template.libraries['custom_simple_tags'] = register
 
 from django_synth.template import SynthTemplate
 
 source = """
-{% load custom_test %}
+{% load custom_simple_tags %}
 {% identity 1 %}
 """
 
