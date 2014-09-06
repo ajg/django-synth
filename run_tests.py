91ab9baa94827e52df8a1b7bc656870204eafd4f
Alvaro J. Genial <genial@alva.ro>
2014-09-05 20:31:01 -0400

    Add custom inclusion tag tests

--- run_tests.py
+++ run_tests.py
@@ -1,9 +1,10 @@
 ##  (C) Copyright 2014 Alvaro J. Genial (http://alva.ro)
 
 import tests.custom_complex_tags
+import tests.custom_inclusion_tags
+import tests.custom_simple_tags
 import tests.django_l10n_library
 import tests.django_static_library
 # TODO: Currently fails on Linux, and might be locale-dependent:
 # import tests.django_tz_library
-import tests.custom_simple_tags
 import tests.trivial_template
---
+++ tests/custom_inclusion_tags.py
@@ -0,0 +1,39 @@
+##  (C) Copyright 2014 Alvaro J. Genial (http://alva.ro)
+
+from django import template
+from django.template import loader
+
+register = template.Library()
+
+
+def _get_template(name, dirs=None):
+    if name == 'experiments/goal.html':
+        return SynthTemplate('[{{ url }}]')
+    else:
+        return get_template(name, dirs)
+
+
+get_template = loader.get_template
+loader.get_template = _get_template
+
+# Adapted from mixcloud/django-experiments:
+@register.inclusion_tag('experiments/goal.html')
+def experiment_goal(goal_name):
+    return {'url': 'http://localhost/' + goal_name}
+
+template.libraries['custom_inclusion_tags'] = register
+
+from django_synth.template import SynthTemplate
+
+source = """
+{% load custom_inclusion_tags %}
+Foo: {% experiment_goal 'foo_experiment' %}
+Bar: {% experiment_goal 'bar_experiment' %}
+"""
+
+expect = '\n\nFoo: [http://localhost/foo_experiment]\nBar: [http://localhost/bar_experiment]\n'
+actual = SynthTemplate(source).render(template.Context({}))
+
+print('expect:', expect)
+print('actual:', actual)
+assert expect == actual
